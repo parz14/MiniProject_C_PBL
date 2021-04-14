f5694f1e70753f40f53a999c6f036e2b43e81896
Sankalp256183 <80450214+Sankalp256183@users.noreply.github.com>
2021-04-14 23:26:35 +0530

    Create billheader.h
    add header file

---
+++ 3_Implementation/inc/billheader.h
@@ -0,0 +1,50 @@
+/**
+ * @file billheader.h
+ * @author sankalp 256183
+ * @brief header file containing function declarations & structure definations
+ * @version 0.1
+ * @date 2021-04-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+// header guard
+
+#ifndef BILL_HEADER
+#define BILL_HEADER
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * @brief structure : containing details of the itmes
+ * 
+ */
+
+typedef struct item_details
+{
+    int quantity;
+    float price;
+} item_details;
+
+/**
+ * @brief function 1 : manages file operation to replace a file word with given word (input data)
+ * 
+ */
+char *replace_word(const char *str, const char *fileword, const char *realword);
+
+/**
+ * @brief function 2: manages conversion of all integer values into string for file operations 
+ * 
+ */
+char *int_to_string_conversion(int integer_no);
+
+ /**
+  * @brief function 2: manages conversion of all float values into string for file operations
+  * 
+  */
+char *float_to_string_conversion(float float_no);
+
+#endif
