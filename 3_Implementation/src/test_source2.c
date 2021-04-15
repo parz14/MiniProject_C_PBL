1fa31ff2d6854db4dae16c0759d8f2c59ed67384
Sankalp256183 <80450214+Sankalp256183@users.noreply.github.com>
2021-04-15 15:12:43 +0530

    Create test_source2.c

---
+++ 3_Implementation/src/test_source2.c
@@ -0,0 +1,33 @@
+#include "testcode.h"
+
+/**
+ * @brief function 2: manages conversion of all integer values into string for file operations, test function defination
+ * 
+ */
+char string[15]; //for conversions functions
+int test = 0;    // for testing int to string
+float test2 = 0; // for testing float to string
+
+int int_to_string_conversion(int integer_no)
+{
+    itoa(integer_no, string, 10);
+    test = atoi(string);
+    if (test == integer_no)
+        return 1;
+    else
+        return 0;
+}
+
+/**
+  * @brief function 2: manages conversion of all float values into string for file operations, test function defination
+  * 
+  */
+int float_to_string_conversion(float float_no)
+{
+    gcvt(float_no, 5, string);
+    test2 = atof(string);
+    if ((int)test2 == (int)float_no)
+        return 1;
+    else
+        return 0;
+}
